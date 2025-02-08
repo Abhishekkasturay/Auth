f4017e212f23de9a1c4f3990749da35be090e6c3
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2025-02-09 02:26:40 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const morgan = require("morgan");
 
 // Load environment variables
